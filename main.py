b6f20438b89fe17108b348d25d4ef8eabf363470
Valkyrio100 <24494940+Valkyrio100@users.noreply.github.com>
2020-12-19 13:26:42 +0100

    Bot updated for discord api update

--- main.py
+++ main.py
@@ -79,27 +79,24 @@ async def on_message(message):
             comm = role[0]
             arg = role[1]
             role_name = role[2]
-            delete_prev = role[3]
             if (cc+comm == call_arguments[0] and arg == call_arguments[1]):
-                add_role = discord.utils.get(message.server.roles, name=role_name)
-                role_list = copy.deepcopy(message.author.roles)
-                user_max_role = get_max_role(role_list)
-                if((user_max_role < conf_call_roles.index(role))):
-                    
-                    if(delete_prev or (final_role and role_name == conf_call_roles[-1][2])):
-                        index = 0
-                        while conf_call_roles[index][2] != add_role.name:
-                            for r in role_list:
-                                if r.name == conf_call_roles[index][2]:
-                                    role_list.remove(r)
-                                    break
-                            index += 1
-
-                    role_list.append(add_role)
-                    await client.replace_roles(message.author,*role_list)
-                    if(logs):
-                        await client.send_message(message.channel, "Role "+add_role.name+" applied to user "+ message.author.name)
-                        print("Role "+add_role.name+" applied to user "+ message.author.name)
+                add_role = discord.utils.get(message.guild.roles, name=role_name)
+                role_list = copy.copy(message.author.roles)
+                if role_name == "Fata Morgana":
+                	door_roles = []
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Rose Manor"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Weeping Manor"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Pig Iron Manor"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Misty Manor"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Fifth Door"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Sixth Door"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Seventh Door"))
+                	door_roles.append(discord.utils.get(message.guild.roles, name="Final Door"))
+                	await message.author.remove_roles(*door_roles)
+                await message.author.add_roles(add_role)
+                if(logs):
+                    await message.channel.send("Role "+add_role.name+" applied to user "+ message.author.name)
+                    print("Role "+add_role.name+" applied to user "+ message.author.name)
 
 #        if(call_arguments[0] == cc+'play'):
 #            if(music_service.voice_client == -1):
